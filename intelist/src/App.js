6aced122d9488ee3b8de31094972f7226d5d32e1
Nisha <nisha.thapa@wheelseye.com>
2022-07-19 23:06:04 +0530

    feat(architecture): built architecture

--- intelist/src/App.js
+++ intelist/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Left from './components/Left/Left';
+import Right from './components/Right/Right';
+import Center from './components/Center/Center';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <Left/>
+        <Center/>
+        <Right/>
     </div>
   );
 }
